0e8bd661d7d8039f3db1b3bb8ee1351f613ec2d9
Mahdi Sharifi <mahdi.sh.twd@gmail.com>
2024-01-05 13:54:24 +0330

    Like loading bug fix.

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,4 +1,4 @@
-import { HeartIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, HeartIcon } from "@heroicons/react/24/outline";
 import { HeartIcon as HeartSolidIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
 import API, { post, responseValidator } from "../utils/api";
@@ -6,11 +6,13 @@ import API, { post, responseValidator } from "../utils/api";
 export default function Like(props) {
     const [liked, setLiked] = useState(props.liked);
     const [count, setCount] = useState(props.count);
+    const [loading, setLoading] = useState(false);
 
     const handleLike = (e) => {
         e.preventDefault();
-
+        setLoading(true);
         post(liked ? API.tweet.dislike(props.id) : API.tweet.like(props.id), {}, (data, status) => {
+            setLoading(false);
             if (responseValidator(status)) {
                 if (liked) {
                     setLiked(false);
@@ -24,13 +26,26 @@ export default function Like(props) {
     };
 
     return (
-        <div className="flex items-center gap-1 cursor-pointer" onClick={handleLike}>
-            {liked ? (
-                <HeartSolidIcon className="w-5 h-5 text-rose-600" />
+        <>
+            {loading ? (
+                <div
+                    onClick={(e) => {
+                        e.preventDefault();
+                    }}
+                    className="animate-spin"
+                >
+                    <ArrowPathIcon className="w-5 h-5 text-gray-500" />
+                </div>
             ) : (
-                <HeartIcon className="w-5 h-5 text-gray-600" />
+                <div className="flex items-center gap-1 cursor-pointer" onClick={handleLike}>
+                    {liked ? (
+                        <HeartSolidIcon className="w-5 h-5 text-rose-600" />
+                    ) : (
+                        <HeartIcon className="w-5 h-5 text-gray-600" />
+                    )}
+                    <p className={`text-sm ${liked ? "text-red-600" : "text-gray-600"}`}>{count}</p>
+                </div>
             )}
-            <p className={`text-sm ${liked ? "text-red-600" : "text-gray-600"}`}>{count}</p>
-        </div>
+        </>
     );
 }
